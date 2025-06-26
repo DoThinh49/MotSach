b6b99701fc00d39165d62c428752c3165a59636b
Do Khac Thinh <dokhacthinh3@gmail.com>
2025-06-26 22:36:24 +0700

    init

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,109 @@
+import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+
+router.get("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Vui lòng điền đầy đủ thông tin" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Mật khẩu phải có ít nhất 6 ký tự" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username phải có ít nhất 3 ký tự" });
+    }
+
+    //kiểm tra xem người dùng đã tồn tại chưa
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email đã tồn tại" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username đã tồn tại" });
+    }
+
+    //get random avt
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    const user = new User({
+      email,
+      username,
+      password,
+      profileImage: "",
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("error in register route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Vui lòng điền đầy đủ thông tin" });
+
+    //check if user ton tai
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(404).json({ message: "Sai thông tin đăng nhập" });
+
+    //check if password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Sai thông tin đăng nhập" });
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      token,
+      user: {
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("error in login route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
