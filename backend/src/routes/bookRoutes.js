13db9af6a7379c41691940ada0cd915fb1e884f4
Do Khac Thinh <dokhacthinh3@gmail.com>
2025-06-26 22:36:24 +0700

    init

---
+++ backend/src/routes/bookRoutes.js
@@ -0,0 +1,107 @@
+import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
+import protectRoute from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    if (!image || !title || !caption || !rating) {
+      return res
+        .status(400)
+        .json({ message: "Vui lòng nhập đầy đủ thông tin" });
+    }
+
+    //tai anh len cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    // luu vao database
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Lỗi khi tạo sách", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Pagination Infinite loading
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(total / limit),
+    });
+  } catch (error) {
+    console.log("Error in get all books", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.json(books);
+  } catch (error) {
+    console.error("Lỗi khi lấy sách của người dùng:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Không tìm thấy sách" });
+
+    // kiểm tra xem User có phải là người tạo ra cuốn sách hay không
+    if (book.user.toString() !== req.user._id.toString())
+      return res.status(401).json({ message: "Unauthorized" });
+
+    // xoa anh khoi cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Lỗi khi xóa hình ảnh khỏi Cloudinary", deleteError);
+      }
+    }
+
+    await book.deleteOne();
+
+    res.json({ message: "Sách đã xóa thành công" });
+  } catch (error) {
+    console.log("Lỗi khi xóa sách", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
