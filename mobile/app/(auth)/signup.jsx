cfa82c5db2bac73b834176448da92841a6c27b20
Do Khac Thinh <dokhacthinh3@gmail.com>
2025-06-27 15:47:54 +0700

    login email password

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform, KeyboardAvoidingView } from "react-native";
+import styles from "../../assets/styles/signup.styles";
 
 export default function Sighnup() {
   return (
-    <View>
-      <Text>Signup Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View>
+        <Text>Signup Screen</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
