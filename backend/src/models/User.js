1167e6bb749e72667396ac95bd082b5b4d2b3cef
Do Khac Thinh <dokhacthinh3@gmail.com>
2025-06-28 17:13:19 +0700

    fixed bcrypt

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
 
 //hash pasword
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("this.password")) return next();
+  if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
